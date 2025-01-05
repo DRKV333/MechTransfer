8996cbc9427e57167b4e9ebbd3c0ff03ce0740e7
Kálmán Vince <30904009+DRKV333@users.noreply.github.com>
2025-01-05 21:06:57 +0100

    Fixed formatting

--- UI/AssemblerHoverUI.cs
+++ UI/AssemblerHoverUI.cs
@@ -3,7 +3,6 @@ using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Terraria;
 using Terraria.GameContent;
 using Terraria.GameContent.UI;
@@ -23,15 +22,15 @@ namespace MechTransfer.UI
         private UIText titleText;
         private UIText stationText;
         private UIText atText;
-        private Item[] fakeInv = new Item[11];
-        private Item[] ingrRow = [];
+        private readonly Item[] fakeInventory = new Item[11];
+        private Item[] ingredientRow = [];
         private float lowestSlot;
 
         private const string NotSetTextKey = "Mods.MechTransfer.UI.Hover.NotSet";
 
         public override void OnInitialize()
         {
-            fakeInv[10] = new Item();
+            fakeInventory[10] = new Item();
 
             titleText = new UIText(Language.GetTextValue(NotSetTextKey));
             titleText.Left.Set(5, 0);
@@ -69,22 +68,26 @@ namespace MechTransfer.UI
             titleText.SetText(text);
             titleText.TextColor = textColor;
 
-            fakeInv[10] = type;
+            fakeInventory[10] = type;
+
             ingredients ??= [];
-            ingrRow = new Item[10 + ingredients.Count];
-            for (int i = 0; i < ingredients.Count; i++) ingrRow[i + 10] = ingredients[i];
+            ingredientRow = new Item[10 + ingredients.Count];
+            for (int i = 0; i < ingredients.Count; i++)
+            {
+                ingredientRow[i + 10] = ingredients[i];
+            }
 
-            itemText.SetText(type.IsAir ? Language.GetTextValue(NotSetTextKey) : fakeInv[10].Name);
-            itemText.TextColor = type.IsAir ? Color.Red : ItemRarity.GetColor(fakeInv[10].rare);
+            itemText.SetText(type.IsAir ? Language.GetTextValue(NotSetTextKey) : fakeInventory[10].Name);
+            itemText.TextColor = type.IsAir ? Color.Red : ItemRarity.GetColor(fakeInventory[10].rare);
             panel.Width.Pixels = itemText.MinWidth.Pixels + TextureAssets.InventoryBack.Value.Width * 0.5f + 25;
-            int rows = (ingrRow.Length - 1) / 10;
-            panel.Width.Pixels = Math.Max(panel.Width.Pixels, (int)Math.Ceiling((ingrRow.Length - 10.0) / (rows==0?1:rows)) * (TextureAssets.InventoryBack.Value.Width + 4) * 0.5f + 20);
-            stationText.Width.Set(panel.GetInnerDimensions().Width-5, 0f);
+            int rows = (ingredientRow.Length - 1) / 10;
+            panel.Width.Pixels = Math.Max(panel.Width.Pixels, (int)Math.Ceiling((ingredientRow.Length - 10.0) / (rows == 0 ? 1 : rows)) * (TextureAssets.InventoryBack.Value.Width + 4) * 0.5f + 20);
+            stationText.Width.Set(panel.GetInnerDimensions().Width - 5, 0f);
 
-            string statnText = "";
+            string stationTextString = "";
             string atTextString = "";
-            float statnHeight = 0;
-            int statnLines = 0;
+            float stationHeight = 0;
+
             if (stations.Count > 0)
             {
                 UIText stationTextBox = new UIText(",");
@@ -92,24 +95,29 @@ namespace MechTransfer.UI
                 atTextString = "@ ";
                 DynamicSpriteFont font = FontAssets.MouseText.Value;
                 float spacing = font.LineSpacing;
-                statnText = "";
+                
+                stationTextString = "";
                 for (int i = 0; i < stations.Count; i++)
                 {
                     int id = stations[i];
-                    if (i != 0) statnText += ",\n";
+                    
+                    if (i != 0)
+                        stationTextString += ",\n";
+                    
                     string stationName = Lang.GetMapObjectName(MapHelper.TileToLookup(id, Recipe.GetRequiredTileStyle(id)));
-                    statnText += stationName;
+                    stationTextString += stationName;
 
                     stationTextBox.SetText(stationName);
                     panel.Width.Pixels = Math.Max(panel.Width.Pixels, stationTextBox.MinWidth.Pixels + stationText.Left.Pixels + 20 + (i < stations.Count - 1 ? semicolonWidth : 0));
                 }
 
-                statnLines = statnText.Split('\n').Length;
-                statnHeight = statnLines * spacing;
+                int stationLines = stationTextString.Split('\n').Length;
+                stationHeight = stationLines * spacing;
             }
+
             atText.SetText(atTextString);
-            stationText.SetText(statnText);
-            panel.Height.Pixels = statnHeight + (TextureAssets.InventoryBack.Value.Width + ((rows == 0 ? 0 : (ingrRow.Length - 10) % rows + 1) * (TextureAssets.InventoryBack.Value.Height + 4))) * 0.5f + 10;
+            stationText.SetText(stationTextString);
+            panel.Height.Pixels = stationHeight + (TextureAssets.InventoryBack.Value.Width + ((rows == 0 ? 0 : (ingredientRow.Length - 10) % rows + 1) * (TextureAssets.InventoryBack.Value.Height + 4))) * 0.5f + 10;
             float top = lowestSlot;
             atText.Top.Set(top, 0f);
             stationText.Top.Set(top, 0f);
@@ -132,17 +140,19 @@ namespace MechTransfer.UI
             lowestSlot = TextureAssets.InventoryBack.Value.Height * 0.5f * Main.UIScale + 10;
             float oldScale = Main.inventoryScale;
             Main.inventoryScale = 0.5f;
-            ItemSlot.Draw(spriteBatch, fakeInv, ItemSlot.Context.InventoryItem, 10, new Vector2(Left.Pixels + 5, Top.Pixels + 30));
-            for (int i=10; i<ingrRow.Length; i++)
+            ItemSlot.Draw(spriteBatch, fakeInventory, ItemSlot.Context.InventoryItem, 10, new Vector2(Left.Pixels + 5, Top.Pixels + 30));
+            
+            for (int i = 10; i < ingredientRow.Length; i++)
             {
                 int j = i - 10;
-                int rows = ((ingrRow.Length - 10) / 10) +1;
-                int cols = (int)Math.Ceiling((ingrRow.Length - 10.0) / rows);
+                int rows = ((ingredientRow.Length - 10) / 10) +1;
+                int cols = (int)Math.Ceiling((ingredientRow.Length - 10.0) / rows);
                 int x = j % cols;
                 int y = j / cols;
-                ItemSlot.Draw(spriteBatch, ingrRow, ItemSlot.Context.InventoryItem, i, new Vector2(Left.Pixels + 5 + (x * 28)+5, Top.Pixels + 56 + (y * 28)+2));
-                lowestSlot = ((y+1) * 28) + TextureAssets.InventoryBack.Value.Height * 0.5f;
+                ItemSlot.Draw(spriteBatch, ingredientRow, ItemSlot.Context.InventoryItem, i, new Vector2(Left.Pixels + 5 + (x * 28) + 5, Top.Pixels + 56 + (y * 28) + 2));
+                lowestSlot = ((y + 1) * 28) + TextureAssets.InventoryBack.Value.Height * 0.5f;
             }
+
             Main.inventoryScale = oldScale;
 
             visible = false;
